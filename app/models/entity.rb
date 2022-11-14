573ec8e9f32a120f25a08bc5587c82a1aee396fd
braincee <kwesi938@gmail.com>
2022-11-14 10:52:53 +0000

    Add validations and relations to User | Group | Entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,8 @@
 class Entity < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
+  
+  validates :amount, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 100 }
+
+    has_many :groups, dependent: :delete_all
+    has_many :entities, dependent: :delete_all
 end
