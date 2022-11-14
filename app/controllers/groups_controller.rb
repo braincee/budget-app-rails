1a61e29a824afe4d97efc364571525a0f489b8f1
braincee <kwesi938@gmail.com>
2022-11-14 14:06:42 +0000

    Add create method | styling to index and new html | update application.css

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,5 +8,18 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def create; end
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    render :new unless @group.save
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
