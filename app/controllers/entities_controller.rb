d519e5c8257a4391e55bded0d60a68148bf8049c
braincee <kwesi938@gmail.com>
2022-11-14 14:43:49 +0000

    Add new and create methods to entities controller

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,20 @@
 class EntitiesController < ApplicationController
-  def index; end
+  def new
+    @entity = Entity.new
+  end
 
-  def create; end
+  def create
+    @groups = params[:spending][:group_id].drop(1)
+    @groups.each do |group|
+      @entity = Entity.new(name: params[:entity][:name], amount: params[:entity][:amount], group_id: group.to_i, user_id: current_user.id)
+      @entity.save
+    end
+    redirect_to group_path(@groups.first.to_i)
+  end
 
-  def new; end
+  private
+
+  def entity_params
+    params.require(:entity).permit(:group_id, :amount, :name)
+  end
 end
