1a92fa5f8b3c9640cc196ca003914d2a33d08a44
braincee <kwesi938@gmail.com>
2022-11-18 18:23:47 +0000

    fix linters

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.new(group_params)
     if @group.save
       redirect_to groups_path(current_user.id)
-    else 
+    else
       render :new
     end
   end
